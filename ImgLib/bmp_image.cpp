c9a2cca9b009a5a6f8b3cf38e90cac761a1480e5
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-05-19 01:07:09 +0500

    bmp_image.cpp

---
+++ ImgLib/bmp_image.cpp
@@ -0,0 +1,96 @@
+#include "bmp_image.h"
+#include "pack_defines.h"
+
+#include <array>
+#include <fstream>
+#include <string_view>
+
+using namespace std;
+
+namespace img_lib {
+    
+PACKED_STRUCT_BEGIN BitmapFileHeader {
+    char sign1 = 'B'; 
+    char sign2 = 'M';
+    uint32_t sum_size;
+    uint32_t reserved_space = 0;
+    uint32_t size_of_both_headers = 54;
+}
+PACKED_STRUCT_END    
+
+PACKED_STRUCT_BEGIN BitmapInfoHeader {
+    uint32_t header_size = 40;
+    uint32_t inf_width;
+    uint32_t inf_height;
+    uint16_t layers = 1;
+    uint16_t bit_per_pixel = 24;
+    uint32_t compression_type = 0;
+    uint32_t bytes_in_data;
+    int32_t horizontal_pixel_per_meter = 11811;
+    int32_t vertical_pixel_per_meter = 11811;
+    int32_t use_colors = 0;
+    int32_t colors = 0x1000000;
+}
+PACKED_STRUCT_END    
+
+// функция вычисления отступа по ширине
+static int GetBMPStride(int w) {
+    return 4 * ((w * 3 + 3) / 4);
+}
+
+bool SaveBMP(const Path& file, const Image& image) {
+    BitmapFileHeader file_header;
+    BitmapInfoHeader info_header;
+    
+    int width = image.GetWidth();
+    int height = image.GetHeight();
+    int stride = GetBMPStride(width);
+
+    file_header.sum_size = stride * height + file_header.size_of_both_headers;
+    info_header.inf_width = width;
+    info_header.inf_height = height;
+    info_header.bytes_in_data = stride * height;
+
+    ofstream out(file, ios::binary);
+    out.write(reinterpret_cast<const char*>(&file_header), sizeof(file_header));
+    out.write(reinterpret_cast<const char*>(&info_header), sizeof(info_header));    
+    vector<char> buff(stride);  
+    for (int y = image.GetHeight() - 1; y >= 0; --y) {
+        const Color* line = image.GetLine(y);
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            buff[x * 3 + 0] = static_cast<char>(line[x].b);
+            buff[x * 3 + 1] = static_cast<char>(line[x].g);
+            buff[x * 3 + 2] = static_cast<char>(line[x].r);
+        }
+        out.write(reinterpret_cast<const char*>(buff.data()), stride);
+    }
+    return out.good();
+}
+
+Image LoadBMP(const Path& file) {
+    ifstream ifs(file, ios::binary);
+    int width, height;
+    ifs.ignore(18);
+    ifs.read(reinterpret_cast<char*>(&width), sizeof(width));
+    ifs.read(reinterpret_cast<char*>(&height), sizeof(height));
+    ifs.ignore(28);
+
+    int stride = GetBMPStride(width);
+    auto color = Color::Black();
+    Image result(stride / 3, height, color);
+    std::vector<char> buff(width * 3);
+
+    for (int y = result.GetHeight() - 1; y >= 0; --y) {
+        Color* line = result.GetLine(y);
+        ifs.read(buff.data(), stride);
+
+        for (int x = 0; x < width; ++x) {
+            line[x].b = static_cast<byte>(buff[x * 3 + 0]);
+            line[x].g = static_cast<byte>(buff[x * 3 + 1]);
+            line[x].r = static_cast<byte>(buff[x * 3 + 2]);
+        }
+    }
+    return result;
+}
+
+}  // namespace img_lib
